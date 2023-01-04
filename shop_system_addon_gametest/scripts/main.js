14c915c641af66a062991f9eba64f9382fd1b63d
はやぶ <78709561+hayabu-olc-ucr@users.noreply.github.com>
2023-01-04 16:14:42 +0900

    データの保存先をtagからdynamic propertyに変更 それに伴い、データが存在しない場合の表示も用意

--- shop_system_addon_gametest/scripts/main.js
+++ shop_system_addon_gametest/scripts/main.js
@@ -1,30 +1,54 @@
+//初期設定のメモ
+//dunamic propertyに金額と品物を記入して保存しておく
+//形式はjson形式で、typeに販売タイプ(foods or goods)、listに販売品データ(name,id,priceの3つの要素)
+
 //UIと基本のモジュールをimportする
 import * as mc from "@minecraft/server";
 import * as mcui from "@minecraft/server-ui";
 
-//ショップのホーム画面のUIを作成
+//ショップのホーム画面のフォームと、その返答を作成
 function shopHome(player, target) {
-    var form = new mcui.ActionFormData()
-        .title("購入できる品物")
-        .body("購入したい品物を選んでください");
-    var tags = target.getTags();
-    for (var i = 0; i < tags.length; i++) {
-        form.button(tags[i]);
-    }
-    form.button("カートを見る");
-    form.button("閉じる");
-    if (player.hasTag('cast')) {
-        form.button("§eキャスト用の画面を開く");
-    }
-    form.show(player).then((response) => {
-        if (response.selection < tags.length) {
-            addCart(player,target);
-        } else if (response.selection === (tags.length + 1)) {
-            checkCart(player,target);
-        } else if (response.selection === (tags.length + 3)) {
-            openCast(player,target);
+    var data = JSON.parse(target.getDynamicProperty("goods"));
+    if (data == null) {
+        var form = new mcui.ActionFormData()
+            .title("購入できる品物")
+            .body("現在、販売している品物はありません！");
+        form.button("閉じる");
+        if (player.hasTag('cast')) {
+            form.button("§eキャスト用の画面を開く");
         }
-    });
+        form.show(player).then((response) => {
+            if (response.selection === 1) {
+                openCast(player, target);
+            }
+        });
+    } else {
+        data = JSON.parse(target.getDynamicProperty("goods"));
+        var form = new mcui.ActionFormData()
+            .title("購入できる品物")
+            .body("購入したい品物を選んでください");
+        for (var i = 0; i < goods.length; i++) {
+            form.button(goods[i]);
+        }
+        form.button("カートを見る");
+        form.button("閉じる");
+        if (player.hasTag('cast')) {
+            form.button("§eキャスト用の画面を開く");
+        }
+        form.show(player).then((response) => {
+            if (response.selection < tags.length) {
+                addCart(player, target, tags[response.selection]);
+            } else if (response.selection === (tags.length + 1)) {
+                checkCart(player, target);
+            } else if (response.selection === (tags.length + 3)) {
+                openCast(player, target);
+            }
+        });
+    }
+}
+
+function addCart(player, target, tag) {
+    tag = tag.split('：');
 }
 
 var scriptOptions = {
