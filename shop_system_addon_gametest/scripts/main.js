e8a7d582e89b11591813302b04780085fcef3a96
はやぶ <78709561+hayabu-olc-ucr@users.noreply.github.com>
2023-01-04 16:23:30 +0900

    変更時に変更し忘れたものの修正と、関数の追加

--- shop_system_addon_gametest/scripts/main.js
+++ shop_system_addon_gametest/scripts/main.js
@@ -1,5 +1,5 @@
 //初期設定のメモ
-//dunamic propertyに金額と品物を記入して保存しておく
+//dynamic propertyに金額と品物を記入して保存しておく
 //形式はjson形式で、typeに販売タイプ(foods or goods)、listに販売品データ(name,id,priceの3つの要素)
 
 //UIと基本のモジュールをimportする
@@ -8,7 +8,7 @@ import * as mcui from "@minecraft/server-ui";
 
 //ショップのホーム画面のフォームと、その返答を作成
 function shopHome(player, target) {
-    var data = JSON.parse(target.getDynamicProperty("goods"));
+    var data = target.getDynamicProperty("goods");
     if (data == null) {
         var form = new mcui.ActionFormData()
             .title("購入できる品物")
@@ -27,8 +27,8 @@ function shopHome(player, target) {
         var form = new mcui.ActionFormData()
             .title("購入できる品物")
             .body("購入したい品物を選んでください");
-        for (var i = 0; i < goods.length; i++) {
-            form.button(goods[i]);
+        for (var i = 0; i < data.list.length; i++) {
+            form.button(data.list[i].name + "：" + data.list[i].price + "オルク");
         }
         form.button("カートを見る");
         form.button("閉じる");
@@ -36,8 +36,8 @@ function shopHome(player, target) {
             form.button("§eキャスト用の画面を開く");
         }
         form.show(player).then((response) => {
-            if (response.selection < tags.length) {
-                addCart(player, target, tags[response.selection]);
+            if (response.selection < data.list.length) {
+                addCart(player, target, data.list[response.selection]);
             } else if (response.selection === (tags.length + 1)) {
                 checkCart(player, target);
             } else if (response.selection === (tags.length + 3)) {
@@ -47,10 +47,17 @@ function shopHome(player, target) {
     }
 }
 
-function addCart(player, target, tag) {
-    tag = tag.split('：');
+//カートに指定されたアイテムを追加して、終わったらカートを表示させる関数を実行
+function addCart(player, target, data) {
+    //カートに追加するための動作を追加予定
 }
 
+//カートを表示して、購入するかまだ探すかを選べる
+function checkCart(player,target) {
+
+}
+
+//コマンドとして追加する用だが、実装できるかわからないため、とりあえず放置
 var scriptOptions = {
     nameSpaces: "test"
 }
